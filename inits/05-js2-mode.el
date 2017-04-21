94426fca05309ac78879c885f2186a3242e0ae6d
Masato Shimizu <ma6ato@gmail.com>
2017-04-21 10:25:04 +0900

    js-mode tab 4 spaces

--- inits/05-js2-mode.el
+++ inits/05-js2-mode.el
@@ -8,7 +8,6 @@
 (add-hook 'js2-mode-hook
           '(lambda ()
               (auto-fill-mode 1)
-              (setq js2-basic-offset 2
+              (setq js2-basic-offset 4
                     indent-tabs-mode nil
                     fill-column 80)))
-
