868811c6e0d0534bc51b59335b0bdff9e2851fdd
Masato Shimizu <masshimi@yahoo-corp.jp>
2018-05-10 10:54:29 +0900

    make-backup-files nil

--- inits/02-files.el
+++ inits/02-files.el
@@ -3,7 +3,10 @@
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+(setq make-backup-files nil)
 (setq backup-inhibited t)
+(setq auto-save-default nil)
+
 (setq next-line-add-newlines nil)
 (setq-default tab-width 4 indent-tabs-mode nil)
 
